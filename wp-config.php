3c4a0aa7b2004e2d741ed144fe37af8e3c6b4337
maxtax <max@mildh.se>
2015-04-23 12:41:14 +0200

    Lunch.nu wordpress

--- wp-config.php
+++ wp-config.php
@@ -16,13 +16,13 @@
 
 // ** MySQL-inställningar - MySQL-uppgifter får du från ditt webbhotell ** //
 /** Namnet på databasen du vill använda för WordPress */
-define('DB_NAME', 'lunch_wp');
+define('DB_NAME', 'linkmast_max');
 
 /** MySQL-databasens användarnamn */
-define('DB_USER', 'root');
+define('DB_USER', 'linkmast_maxm');
 
 /** MySQL-databasens lösenord */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'slfnKExIQ3pW');
 
 /** MySQL-server */
 define('DB_HOST', 'localhost');
@@ -42,14 +42,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'tOpmlH1G5&4BOV*a;U4h3`EbV+;;y7Jl}3zVw7zfh?.zGLmhk~+)O4e;$~Aa>0R%');
-define('SECURE_AUTH_KEY',  '66|a,-gU$h^jm=I+k:Y2{oEG-%g?(z# Ok,8o=Ynb[uUP+*| C+Y=6|ybym@uqYK');
-define('LOGGED_IN_KEY',    'TFa/I-SH:`lOn%?AJ)]gI_B:74U@6vPn1+32Yctw7M(p $+q5p+x9#x@)b[X.|5(');
-define('NONCE_KEY',        '<oNum{HIv=&+mw0:[-CQul/o- B.QF4tNOrCRxftTX6nZ#--?20:$V6I->J/x+Sj');
-define('AUTH_SALT',        ':?$bbu0xsdH.%-MC!_X(+[@v9(N~ez+s?$q]~.@|(|nV|@n_p5S-+Qn<#-p=bWi,');
-define('SECURE_AUTH_SALT', '9{:w]+YHRggU$P~I9!?^zz}[_ nd9N>tMHoh+~EBOxF30DE2A-2efiCq9XHk|dx$');
-define('LOGGED_IN_SALT',   'mTr8iD?*.vC<#r4rbM&x7Duv3N~~hikGvmG+7P2d iPfk,tX5#MxJ%^GR)(sG/+:');
-define('NONCE_SALT',       'OCV-$w7[K2*&<NRm3@H{*:,C=<u-CzyJ+<jp=]}<@uHw78@QX|r%HkBt:u%A/`?w');
+define('AUTH_KEY',         '805myWZU>-dsOou>|i}EKzo;;6&_Y8Uyr4h#cq84AuJ$~NQ?>W%NE|z]q!~,Qh,}');
+define('SECURE_AUTH_KEY',  ',`H{W3u_v%R;wN&G61kcyW1bvy+T1!ib-Px@3mn}3S7#g4PPi>(BFtlQ>HP7V{X,');
+define('LOGGED_IN_KEY',    'AnF_UIGP-9iz%mIU,W;mT*Ye6#j_}>_P>z/;m=_.!+8So>UTab@UO2g11)v9i!YO');
+define('NONCE_KEY',        '*|>Zc8_-fL) 7N^+D{i(N-sNi{-QA~9?^Lwf[c96Bba7(|i!^U@0!R%43;hI8l94');
+define('AUTH_SALT',        '3+FURzc>.>|3FvF?ubRxMkyJqd:u5b#KHzf$~SSiXm7`N:0Nc|F@P2X($(*BTyLs');
+define('SECURE_AUTH_SALT', 'WQgtQ{qPy+F1g>0E@+tiho1x)=-Ro[?+$/~/]o-hE5w9XbPa)kEqKKx*gZz}5&|y');
+define('LOGGED_IN_SALT',   'Sj9|I>G#F|ld}jhz[*M^f)5E-u}98qs),hOL3xTK{<s(^Z%v%SSn]b|,m>P7)&*|');
+define('NONCE_SALT',       'k~{aTMk)r018-Lk>^rtzZdU-skp4D2NMS_+R87||GsPGSGK`Qv.mE!q57V+|qE|V');
 
 /**#@-*/
 
@@ -59,7 +59,7 @@ define('NONCE_SALT',       'OCV-$w7[K2*&<NRm3@H{*:,C=<u-CzyJ+<jp=]}<@uHw78@QX|r%
  * Du kan ha flera installationer i samma databas om du ger varje installation ett unikt
  * prefix. Endast siffror, bokstäver och understreck!
  */
-$table_prefix  = 'lunch_';
+$table_prefix  = 'wplunch_';
 
 /** 
  * För utvecklare: WordPress felsökningsläge. 
